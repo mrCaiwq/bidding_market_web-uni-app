f571b53cacda6cbfec08a57ab88b3b5b033177fa
mr_caiwq <mr_caiwq>
2019-04-17 18:35:20 +0800

    complete home-page

---
+++ src/pages/index/components/recommend.vue
@@ -0,0 +1,116 @@
+<template>
+	<view class="recommend">
+		<view class="recommend-box">
+			<view class="recommend-top-bar">首页推荐</view>
+			<view class="replace" @click="replace()" v-if="productList.length >= 10">
+				<text>换一组</text>
+				<image src="/static/replace.png" mode="scaleToFill"></image>
+			</view>
+			<view class="product-box" v-for="(product, index) in showList" :key="index">
+				<image :src="product.banner_url" mode="scaleToFill"></image>
+				<view class="product-msg">
+					<text class="product-name">{{ product.name }}</text>
+					<text class="money">￥{{ product.curr_price }}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations } from 'vuex';
+export default {
+	data() {
+		return {
+			productList: [
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				},
+				{
+					banner_url: require('../../../static/example.png'),
+					name: '大宝SOD22蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜蜜',
+					curr_price: '1000'
+				}
+			],
+			showList: []
+		};
+	},
+	onLoad() {},
+	onReady() {
+		this.product();
+	},
+	computed: {},
+	methods: {
+		replace() {
+			let count = this.productList.length;
+			let productList = this.productList.concat();
+			this.showList = [];
+			for (let i = 0; i <= 10; i++) {
+				let index = parseInt(Math.random() * count) + i;
+				this.showList.push(productList[index]);
+				productList[index] = productList[i];
+				count--;
+			}
+		},
+		product() {
+			this.showList = this.productList.slice(0, 10);
+		}
+	}
+};
+</script>
+
+<style>
+@import '../../../static/style/recommendComponent.scss';
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,60 +1,61 @@
 <template>
 	<view class="page index">
-		<view class="category-box">
-			<view class="category" v-for="(category,index) in categoryList" :key=index>
-				<image class="photo" :src="category.name ? '../../static/example.png' : '../../static/stay-tuned.png'" mode="scaleToFill"></image>
-				<text>{{category.name}}</text>
-			</view>
-			<button class="more-category" hover-class="button-hover">全部分类</button>
-		</view>
+		<categories></categories>
+		<recommend></recommend>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			categoryList: [{}, {}, {}, {}, {}, {}, {}, {}]
-		};
-	},
-	onReady() {
-		// this.$checkToken()
-	},
-	onNavigationBarButtonTap(i) {
-		if (i.index == 0) {
+	import categories from "./components/categories.vue"
+	import recommend from "./components/recommend.vue"
+	export default {
+		components: {
+			categories,
+			recommend
+		},
+		data() {
+			return {
+
+			};
+		},
+		onReady() {
+			// this.$checkToken()
+		},
+		onNavigationBarButtonTap(i) {
+			if (i.index == 0) {
+				uni.showModal({
+					title: '发布新拍卖',
+					content: '这是一个弹窗',
+					success: function() {
+						console.log(1111);
+					}
+				});
+			}
+		},
+		onPullDownRefresh() {
 			uni.showModal({
-				title: '发布新拍卖',
+				title: '提示',
 				content: '这是一个弹窗',
 				success: function() {
 					console.log(1111);
+					uni.stopPullDownRefresh();
 				}
 			});
-		}
-	},
-	onPullDownRefresh() {
-		uni.showModal({
-			title: '提示',
-			content: '这是一个弹窗',
-			success: function() {
-				console.log(1111);
-				uni.stopPullDownRefresh();
+		},
+		methods: {
+			login() {
+				uni.navigateTo({
+					url: '../login/login',
+					success() {},
+					fail: err => {
+						console.log(JSON.stringify(err));
+					}
+				});
 			}
-		});
-	},
-	methods: {
-		login() {
-			uni.navigateTo({
-				url: '../login/login',
-				success() {},
-				fail: err => {
-					console.log(JSON.stringify(err));
-				}
-			});
 		}
-	}
-};
+	};
 </script>
 
 <style>
-@import '../../static/style/index.scss';
+	@import '../../static/style/index.scss';
 </style>
